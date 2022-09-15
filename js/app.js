8e176b455d882efd51f93124f84a411d4ed3f5a4
Evander33 <33woods33e@gmail.com>
2022-09-14 19:34:23 -0700

    button info added

--- js/app.js
+++ js/app.js
@@ -1,2 +1,5 @@
 $(document).foundation();
-$button;
+
+localStorage.setItem("value1", 123);
+localStorage.setItem("value2", "abc");
+localStorage.setItem("state", JSON.stringify({ a: 1, b: 2, c: 3 }));
